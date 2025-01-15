27a8adb844e8cf35b18764fa12d58d1f81645db5
layzimayner <vasyahunta@gmail.com>
2025-01-15 18:17:46 +0200

    add model

--- src/main/java/com/example/demo/model/Rental.java
+++ src/main/java/com/example/demo/model/Rental.java
@@ -1,4 +1,44 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Rentals")
+@SQLDelete(sql = "UPDATE rentals SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate rentalDate;
+
+    @Column(nullable = false)
+    private LocalDate returnDate;
+
+    @Column(nullable = false)
+    private LocalDate actualReturnDate;
+
+    @Column(nullable = false)
+    private Car car;
+
+    @Column(nullable = false)
+    private User user;
+
+    @Column(nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
+    private boolean isDeleted;
 }
